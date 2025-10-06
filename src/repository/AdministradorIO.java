9f62ab1a82e03607edd0817ab5e47eb42bb0a048
Tomas Robino <tomaserobino@gmail.com>
2025-10-06 12:22:39 +0200

    cosas

---
+++ src/repository/AdministradorIO.java
@@ -0,0 +1,88 @@
+package repository;
+
+import model.Asignatura;
+import model.Estudiante;
+import model.Matricula;
+
+import java.io.File;
+
+public class AdministradorIO implements BDInterfaz {
+    private static AdministradorIO instance;
+    private final TiposPersistencia tipo;
+    private File archivo;
+    private BDInterfaz bd;
+
+    private AdministradorIO(TiposPersistencia tipo, File archivo) {
+        this.tipo = tipo;
+        this.archivo = archivo;
+        switch (tipo) {
+            case BIN -> bd = new BIN(archivo);
+            case CSV -> bd = new CSV(archivo);
+            case SQL -> bd = new SQL();
+            case XML -> bd = new XML(archivo);
+        }
+    }
+
+    private AdministradorIO(TiposPersistencia tipo) {
+        this.tipo = tipo;
+    }
+
+    public static AdministradorIO getInstance(TiposPersistencia tipo, String archivo) {
+        if (instance == null) {
+            if (tipo == TiposPersistencia.SQL) {
+                instance = new AdministradorIO(tipo);
+            } else {
+                File file = new File(archivo);
+                // error
+                if (!file.exists()) return null;
+
+                instance = new AdministradorIO(tipo, file);
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public Object find(int id) {
+        return bd.find(id);
+    }
+
+    @Override
+    public boolean insert(Matricula matricula) {
+        return bd.insert(matricula);
+    }
+
+    @Override
+    public boolean insert(Estudiante estudiante) {
+        return bd.insert(estudiante);
+    }
+
+    @Override
+    public boolean insert(Asignatura asignatura) {
+        return bd.insert(asignatura);
+    }
+
+    @Override
+    public boolean update(Matricula matricula) {
+        return bd.update(matricula);
+    }
+
+    @Override
+    public boolean update(Estudiante estudiante) {
+        return bd.update(estudiante);
+    }
+
+    @Override
+    public boolean update(Asignatura asignatura) {
+        return bd.update(asignatura);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return bd.delete(id);
+    }
+
+    public TiposPersistencia getTipo() {
+        return tipo;
+    }
+}
---
+++ src/repository/XML.java
@@ -0,0 +1,53 @@
+package repository;
+
+import model.Asignatura;
+import model.Estudiante;
+import model.Matricula;
+
+import java.io.File;
+
+public class XML implements BDInterfaz{
+    public XML(File archivo) {
+
+    }
+
+    @Override
+    public Object find(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean insert(Matricula matricula) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(Estudiante estudiante) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(Asignatura asignatura) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Matricula matricula) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Estudiante estudiante) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Asignatura asignatura) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
