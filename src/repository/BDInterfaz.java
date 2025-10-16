726faffb749f93198662af66ea8a9933435c5e38
Tomas Robino <tomaserobino@gmail.com>
2025-10-16 13:44:42 +0200

    removed exception from update

--- src/repository/BDInterfaz.java
+++ src/repository/BDInterfaz.java
@@ -9,6 +9,6 @@ public abstract class BDInterfaz {
 
     abstract String find(int id) throws IOException;
     abstract void insert(Model model) throws IOException;
-    abstract boolean update(Model model) throws IOException;
+    abstract boolean update(Model model);
     abstract boolean delete(int id) throws IOException;
 }
