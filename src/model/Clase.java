8e30b40926756844493c72ff8949d2a22ea74c28
Tomas Robino <tomaserobino@gmail.com>
2025-12-09 17:16:47 +0100

    models for Clase and Profesor

---
+++ src/model/Clase.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Clase extends Model {
+    private final String nombre;
+    private final String horario;
+    private ArrayList<Profesor> profesores;
+
+    public Clase(int id, String nombre, String horario, ArrayList<Profesor> profesores) {
+        this.id = id;
+        this.nombre = nombre;
+        this.horario = horario;
+        this.profesores = profesores;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public ArrayList<Profesor> getProfesores() {
+        return profesores;
+    }
+
+    public void setProfesores(ArrayList<Profesor> profesores) {
+        this.profesores = profesores;
+    }
+}
---
+++ src/model/Profesor.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Profesor extends Model {
+    private final String nombre;
+    private final String especialidad;
+    private ArrayList<Clase> clases;
+
+    public Profesor(int id, String nombre, String especialidad, ArrayList<Clase> clases) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.clases = clases;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public ArrayList<Clase> getClases() {
+        return clases;
+    }
+
+    public void setClases(ArrayList<Clase> clases) {
+        this.clases = clases;
+    }
+}
