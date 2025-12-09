68a01b9f2c0b1ac1c397127d3fb033bb4d87fad2
Tomas Robino <tomaserobino@gmail.com>
2025-12-09 15:54:44 +0100

    use of try-with-resources in MatriculaRepository

--- src/repository/MatriculaRepository.java
+++ src/repository/MatriculaRepository.java
@@ -8,18 +8,14 @@ import java.util.ArrayList;
 public class MatriculaRepository extends Database<Matricula, Integer> {
     @Override
     public Matricula find(Integer id) {
-        try {
+        try(
             Connection conexion = DriverManager.getConnection(uri, usuario, password);
-
             Statement st = conexion.createStatement();
             // PreparedStatement is not necessary because "id" is an integer
             ResultSet rs = st.executeQuery("SELECT * FROM matriculas WHERE id = " + id);
+        ) {
             rs.next();
-            Matricula matricula = new Matricula(rs.getInt("id"), rs.getDouble("nota"), rs.getString("fecha") );
-            rs.close();
-            st.close();
-            conexion.close();
-            return matricula;
+            return new Matricula(rs.getInt("id"), rs.getDouble("nota"), rs.getString("fecha") );
         } catch (SQLException e) {
             e.printStackTrace();
         }
