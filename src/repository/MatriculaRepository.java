b57381259083e9db71503eda7e9d6a9ac60b64e0
Tomas Robino <tomaserobino@gmail.com>
2025-12-09 16:50:33 +0100

    Matricula insert() now checks whether the student exists

--- src/repository/MatriculaRepository.java
+++ src/repository/MatriculaRepository.java
@@ -47,19 +47,26 @@ public class MatriculaRepository extends Database<Matricula, Integer> {
             return false;
         }
 
-        try (
-            Connection conexion = DriverManager.getConnection(uri, usuario, password);
-            PreparedStatement ps = conexion.prepareStatement("INSERT INTO matriculas (estudiante_id, nota, fecha) VALUES (?, ?, ?)")
-        ) {
-            ps.setInt(1, model.getEstudiante_id());
-            ps.setDouble(2, model.getNota());
-            ps.setString(3, model.getFecha());
-            ps.executeUpdate();
-            return true;
+        try (Connection conexion = DriverManager.getConnection(uri, usuario, password)) {
+            try (
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM estudiantes WHERE id = " + model.getEstudiante_id())
+            ) {
+                if (!rs.next()) {
+                    return false;
+                }
+            }
+
+            try (PreparedStatement ps = conexion.prepareStatement("INSERT INTO matriculas (estudiante_id, nota, fecha) VALUES (?, ?, ?)")) {
+                ps.setInt(1, model.getEstudiante_id());
+                ps.setDouble(2, model.getNota());
+                ps.setString(3, model.getFecha());
+                ps.executeUpdate();
+                return true;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return false;
     }
 
